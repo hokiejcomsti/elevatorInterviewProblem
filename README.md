0e57c103603e66e003cbea146b3e12902597ff28
hokiejcomsti <jcomsti@gmail.com>
2023-07-26 07:48:09 -0600

    clean up and readme

--- README.md
+++ README.md
@@ -1,19 +1,17 @@
 ## ASSUMPTIONS
 
 - The parameters "start=" and "floor=" are required but can be in any order
-- The floor parameter are:
-  - Integers
+- The start parameter is:
+  - Only a single integer
+- The floor parameter contains:
+  - Only integers
   - Comma delimited
 
 ## TO RUN
 
-Navigate to the src the directory and compile: 
-```
-javac -cp . main/java/*.java
-```
-
-You will be able to run the application using the following:
-```
-java -cp . main/java/ElevatorTravelTimeCalc  start=<starting floor> floor=<floor1>,<floor2>,...,<floorn>
-```
+To run the application:
+1. Navigate to the src directory and compile:
+   - ```javac -cp . main/java/*.java```
+2. From the src, execute the calculator using the following:
+   - ```java -cp . main/java/ElevatorTravelTimeCalc  start=<starting floor> floor=<floor1>,<floor2>,...,<floorn>```
 
--- src/main/java/ElevatorTravelTimeCalc.java
+++ src/main/java/ElevatorTravelTimeCalc.java
@@ -8,12 +8,6 @@ public class ElevatorTravelTimeCalc {
     private static final String FLOORS_TO_VISIT_LBL = "floor=";
 
     public static void main(String[] args) {
-        // elevator start=12 floor=2,9,1,32
-        if (args.length < 2) {
-            System.out.println("INCORRECT SYNTAX: java -cp . main/java/ElevatorTravelTimeCalculator start=<startingFloor> floor=<floor1>,<floor2>,<floorn>");
-            return;
-        }
-
         try {
             List<String> streamArgs = Arrays.asList(args);
             String startingFloor = TravelTimeService.findParameter(STARTING_FLOOR_LBL, streamArgs);
@@ -35,8 +29,8 @@ public class ElevatorTravelTimeCalc {
 
             int travelTime = TravelTimeService.calTravelTime(intStartFloor, listOfFloors);
             System.out.println("Total travel time: " + travelTime + " seconds");
-        } catch (NumberFormatException e) {
-            System.out.println("Invalid input! Please provide valid integer values for the floors.");
+        } catch (Exception e) {
+            System.out.println("Please provide valid integer values for start and floors");
         }
     }
 }
